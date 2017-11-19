304f472623dba896de54d64b96d7ab9db9e627aa
AngryJayFox <angryjayfox@gmail.com>
2017-11-19 23:00:08 +0700

    add libx264... not working yet

--- code.py
+++ code.py
@@ -14,6 +14,7 @@ parser.add_argument('-if', '--ifile', action='store', help='directory to file fo
 parser.add_argument('-ch', '--check', default=False, action='store_true', help='check starting')
 parser.add_argument('--shared', default=False, action='store_true', help='switch to "shared" build. "static" in default configuration')
 parser.add_argument('-p', '--path', action='store', help='destination path')
+parser.add_argument('--libx264', default=False, action='store_true', help='download and install libx264')
 
 def prepare():
     try:
@@ -105,6 +106,63 @@ def configure(args):
         sys.exit(1)
 
 
+def libx264(args):
+    if args.download is False:
+        os.chdir('./ffmpeg_source')
+        print('workingdir is changed to "./ffmpeg_source"')
+    if args.path is None:
+        varhome = os.environ["HOME"]
+    else:
+        varhome = args.path
+    d = os.listdir('.')
+    if 'x264' in d:
+        shutil.rmtree('x264')
+    os.mkdir('x264')
+    gitcom = ['git', '-C', 'x264', 'pull', '2>' '/dev/null', '||', 'git', 'clone',
+              '--depth', '1', 'http://git.videolan.org/git/x264']
+    try:
+        print('trying to download libx264 sources')
+        subprocess.check_call(gitcom)
+        print('success!')
+    except:
+        print('downloading libx264 sources via git failed')
+        sys.exit(1)
+    os.chdir('x264')
+    print('workingdir is changed to "x264"')
+    path = '{0}/bin:{1}'.format(varhome, varpath)
+    os.environ['PATH'] = path
+    pkgpath = '{0}/ffmpeg_build/lib/pkgconfig'.format(varhome)
+    os.environ['PKG_CONFIG_PATH'] = pkgpath
+    confcode = ['./configure', '--prefix={0}/ffmpeg_build'.format(varhome), '--bindir={0}/bin', '--enable-static']
+    try:
+        print('chmod to configure...')
+        st = os.stat('configure')
+        os.chmod('configure', st.st_mode|0o111)
+        print('success!')
+    except:
+        print('chmod to "configure" fail')
+        sys.exit(1)
+    try:
+        print('trying to configure...')
+        subprocess.check_call(confcode)
+        print('success!')
+    except:
+        print('fail!')
+        sys.exit(1)
+    path = '{0}/bin:{1}'.format(varhome, varpath)
+    os.environ['PATH'] = path
+    try:
+        print('trying to make...')
+        subprocess.check_call(['make'])
+        print('success!')
+        print('trying to make install...')
+        subprocess.check_call(['make', 'install'])
+        print('success!')
+    except:
+        print('make error')
+        sys.exit(1)
+
+
 def checking(args):
     if args.path is None:
         varhome = os.environ["HOME"]
@@ -132,6 +190,8 @@ def main():
         unpack()
     if args.configure is True:
         configure(args)
+    if args.libx264 is True:
+        libx264(args)
     if args.check is True:
         checking(args)
     if args.ifile is not None:
